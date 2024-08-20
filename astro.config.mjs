280dbfe929fb79684b6a21a5ea79b2f23ce20743
zrr1999 <2742392377@qq.com>
2024-08-20 22:07:53 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,21 @@
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import vue from "@astrojs/vue";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import AstroPWA from "@vite-pwa/astro";
 import pagefind from "astro-pagefind";
+// import mdx from "@astrojs/mdx";
 
 import remarkMath from "remark-math";
 import remarkCodeTitles from "remark-code-titles";
+// import remarkToc from "remark-toc";
+// import remarkCollapse from "remark-collapse";
 
 import rehypeSlug from "rehype-slug";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeToc from "rehype-toc";
 import rehypeKatex from "rehype-katex";
-
-// import remarkToc from "remark-toc";
-// import remarkCollapse from "remark-collapse";
-// import sitemap from "@astrojs/sitemap";
 
 import { SITE } from "./src/config";
 
@@ -25,7 +23,7 @@ export default defineConfig({
   prefetch: true,
   site: SITE.website,
   integrations: [
-    mdx(),
+    // mdx(),
     sitemap(),
     vue(),
     react(),
@@ -45,16 +43,15 @@ export default defineConfig({
       rehypeKatex,
     ],
     shikiConfig: {
-      // For more themes, visit https://shiki.style/themes
       themes: { light: "min-light", dark: "night-owl" },
       wrap: true,
     },
   },
-  vite: {
-    optimizeDeps: {
-      exclude: ["@resvg/resvg-js"],
-    },
-  },
+  // vite: {
+  //   optimizeDeps: {
+  //     exclude: ["@resvg/resvg-js"],
+  //   },
+  // },
   // scopedStyleStrategy: "where",
   // experimental: {
   //   contentLayer: true,
